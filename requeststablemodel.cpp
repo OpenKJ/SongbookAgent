c1b08709fd7ecf7576e98dc77420ef559247cf9f
Isaac Lightburn <isaac@openkj.org>
2018-11-27 20:23:27 -0600

    Fix key column alignment and select mode

--- requeststablemodel.cpp
+++ requeststablemodel.cpp
@@ -83,7 +83,7 @@ QVariant RequestsTableModel::data(const QModelIndex &index, int role) const
         switch(index.column())
         {
         case KEY:
-            return Qt::AlignCenter;
+            return Qt::AlignHCenter;
         default:
             return Qt::AlignLeft;
         }
--- version.h
+++ version.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_BUILD 4
-#define VERSION_STRING "1.0.4"
+#define VERSION_BUILD 5
+#define VERSION_STRING "1.0.5"
 #define VERSION_BRANCH "stable"
 
 #endif //VERSION_H
