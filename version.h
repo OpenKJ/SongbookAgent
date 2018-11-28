8e70b46a019efd669c92177f20b2e2aee7e22fba
Isaac Lightburn <isaac@openkj.org>
2018-11-28 02:23:00 -0600

    Updating development branch versioning

--- version.h
+++ version.h
@@ -2,9 +2,9 @@
 #define VERSION_H
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 0
-#define VERSION_BUILD 21
-#define VERSION_STRING "1.0.21"
-#define VERSION_BRANCH "stable"
+#define VERSION_MINOR 1
+#define VERSION_BUILD 1
+#define VERSION_STRING "1.1.1"
+#define VERSION_BRANCH "unstable"
 
 #endif //VERSION_H
