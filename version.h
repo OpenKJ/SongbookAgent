4103978c035c6e2a8bd6611f614ff3bfdec03016
Isaac Lightburn <isaac@openkj.org>
2018-11-28 03:15:30 -0600

    bleh

--- version.h
+++ version.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_BUILD 22
-#define VERSION_STRING "1.0.22"
+#define VERSION_BUILD 23
+#define VERSION_STRING "1.0.23"
 #define VERSION_BRANCH "stable"
 
 #endif //VERSION_H
