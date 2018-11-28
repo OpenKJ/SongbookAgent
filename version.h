618544a19807080b077ac076054e7f09a0f7cb11
Isaac Lightburn <isaac@openkj.org>
2018-11-27 23:20:34 -0600

    Working on macOS auto builds

--- version.h
+++ version.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_BUILD 12
-#define VERSION_STRING "1.0.12"
+#define VERSION_BUILD 13
+#define VERSION_STRING "1.0.13"
 #define VERSION_BRANCH "stable"
 
 #endif //VERSION_H
