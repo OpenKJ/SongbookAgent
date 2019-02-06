d3da9b58f56abe188ea6cdbc3e071081b4e5d983
Isaac Lightburn <isaac@openkj.org>
2019-02-05 19:26:11 -0600

    Force build to bump version and test updater

--- version.h
+++ version.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 1
-#define VERSION_BUILD 12
-#define VERSION_STRING "1.1.12"
+#define VERSION_BUILD 13
+#define VERSION_STRING "1.1.13"
 #define VERSION_BRANCH "unstable"
 
 #endif //VERSION_H
